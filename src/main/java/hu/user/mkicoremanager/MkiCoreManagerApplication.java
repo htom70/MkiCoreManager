324ef904900512df99f2204abecc1bb48566877a
Tom <hering.tamas@gmail.hu>
2021-09-23 16:17:57 +0200

    Initial commit

---
+++ src/main/java/hu/user/mkicoremanager/MkiCoreManagerApplication.java
@@ -0,0 +1,13 @@
+package hu.user.mkicoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkiCoreManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MkiCoreManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/user/mkicoremanager/MkiCoreManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.user.mkicoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkiCoreManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
